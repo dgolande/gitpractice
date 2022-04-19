eb6ec156d3c8cea53a2da6eab16764e696d2e70c
dj <djinnn311@gmail.com>
2022-04-19 20:46:18 +0530

    Develop branch USA changes

---
+++ src/Aone/GoMechanic.java
@@ -0,0 +1,11 @@
+package Aone;
+
+public class GoMechanic {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("CAR");
+
+	}
+
+}
---
+++ src/Aone/Zigwheels.java
@@ -0,0 +1,12 @@
+package Aone;
+
+public class Zigwheels {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Car");
+		
+
+	}
+
+}
